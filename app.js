ad969896da951a893e417a43afca404414a98684
John <johnm589@yahoo.com>
2015-11-24 19:38:24 -0800

    trying more

--- app.js
+++ app.js
@@ -62,3 +62,6 @@ app.use('/', favoriteRoutes)
 app.listen(port, function(){
 	console.log("Server Running!", port)
 })
+app.set('port', (process.env.PORT|| 5000))
+
+app.use(express.static('public'))
